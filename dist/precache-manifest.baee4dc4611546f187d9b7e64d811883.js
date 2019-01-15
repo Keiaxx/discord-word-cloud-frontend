7a4b9aa27a4370f342e735cf87da8a0d48112e1b
Adrian Gose <adrian@gose.pw>
2019-01-14 20:08:21 -0600

    Update ghpages

---
+++ dist/precache-manifest.baee4dc4611546f187d9b7e64d811883.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/discord-word-cloud-frontend/robots.txt"
+  },
+  {
+    "revision": "66352e7ef44e6588cb70",
+    "url": "/discord-word-cloud-frontend/js/chunk-vendors.f12b36a3.js"
+  },
+  {
+    "revision": "8f1ca1aae41c021f0f2a",
+    "url": "/discord-word-cloud-frontend/js/app.458d52c1.js"
+  },
+  {
+    "revision": "8533b24812b39ad4ee58fdbe8ee63331",
+    "url": "/discord-word-cloud-frontend/index.html"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/discord-word-cloud-frontend/img/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "b6e6159efa7453d7abdc506ca5e1d377",
+    "url": "/discord-word-cloud-frontend/img/file.b6e6159e.svg"
+  },
+  {
+    "revision": "6459849cb7031e486cd56958f0f1ae07",
+    "url": "/discord-word-cloud-frontend/img/close.6459849c.svg"
+  },
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/discord-word-cloud-frontend/img/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "66352e7ef44e6588cb70",
+    "url": "/discord-word-cloud-frontend/css/chunk-vendors.79e5e4b1.css"
+  },
+  {
+    "revision": "8f1ca1aae41c021f0f2a",
+    "url": "/discord-word-cloud-frontend/css/app.877a514a.css"
+  }
+];
